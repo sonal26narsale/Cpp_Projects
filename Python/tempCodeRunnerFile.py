b5c92639ae1db2f8f5289b1be114a75411136bf4
sonal26narsale <98825471+sonal26narsale@users.noreply.github.com>
2022-08-08 20:04:50 +0530

    Add files via upload

---
+++ Python/tempCodeRunnerFile.py
@@ -0,0 +1,9 @@
+ist1.remove(c)
+            list2.remove(c)
+
+            # concatenation of two list elements with *
+            # * is act as border mark here
+            list3 = list1 + ["*"] + list2
+
+            # return the concatenated list with True flag
+            return [list3, 

9908ea226cf77f8c94653dfbdcfbe8db1a66a933
sonal26narsale <98825471+sonal26narsale@users.noreply.github.com>
2023-10-18 13:22:25 +0530

    Delete Python directory

--- Python/tempCodeRunnerFile.py
+++
@@ -1,9 +0,0 @@
-ist1.remove(c)
-            list2.remove(c)
-
-            # concatenation of two list elements with *
-            # * is act as border mark here
-            list3 = list1 + ["*"] + list2
-
-            # return the concatenated list with True flag
-            return [list3, 
